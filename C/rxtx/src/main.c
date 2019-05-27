527b3e8b31b9ffee4d17726424ae8c23e8b54a3b
Guy Wilson <wilson.guy@gmail.com>
2019-05-27 15:40:20 +0100

    Got Rx-Tx serial comms working

--- C/rxtx/src/main.c
+++ C/rxtx/src/main.c
@@ -3,6 +3,7 @@
 #include <fcntl.h> 
 #include <string.h>
 #include <termios.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -10,173 +11,181 @@
 #include "rxtxmsgdef.h"
 
 #ifndef SERIAL_PORT
-#define SERIAL_PORT			"/dev/cu.wchusbserial1410"
+#define SERIAL_PORT			"/dev/tty.usbmodem14101"
 #endif
 
-/*
-** Request frame (6 - 80 bytes)
-** <START><LENGTH><MSG_ID><CMD><DATA (0 - 74)><CHECKSUM><END>
-**                 |                        |
-**                  --included in checksum--
-**
-** ACK response frame
-** <START><LENGTH><MSG_ID><ACK><DATA><CHECKSUM><END>
-**
-** NAK response frame
-** <START><MSG_ID><NAK><ERR_CODE><END>
-**
-**
-** The checksum is calculated as follows:
-**
-** The sender adds all the data bytes plus the cmd and msg id, taking
-** just the least significant byte and subtracting that from 0xFF gives
-** the checksum.
-**
-** The receiver adds all the data bytes plus the cmd, msg id and checksum,
-** the least significant byte should equal 0xFF if the checksum is valid.
-*/
-
-#define NUM_REQUEST_FRAME_BYTES		  6
-#define MAX_DATA_LENGTH				 74
-#define MAX_REQUEST_MESSAGE_LENGTH	(NUM_REQUEST_FRAME_BYTES + MAX_DATA_LENGTH)
-
-#define MAX_CMD_FRAME_LENGTH		 76		// Data + msgID + cmd
-
-#define MSG_CHAR_START				0x7E
-#define MSG_CHAR_END				0x81
-
-#define MSG_CHAR_ACK				0x06
-#define MSG_CHAR_NAK				0x15
-
-#define MSG_NAK_UNKNOWN_CMD			0x01
-#define MSG_NAK_INVALID_CHECKSUM	0x02
-#define MSG_NAK_DATA_ERROR			0x04
-#define MSG_NAK_DATA_OVERRUN		0x08
-
-#define RX_STATE_START				0x01
-#define RX_STATE_LENGTH				0x02
-#define RX_STATE_MSGID				0x03
-#define RX_STATE_CMD				0x04
-#define RX_STATE_DATA				0x05
-#define RX_STATE_CHECKSUM			0x06
-#define RX_STATE_END				0x07
-
-#define RX_CMD_TPH					0x01
-#define RX_CMD_ANEMOMETER			0x02
-#define RX_CMD_RAINGUAGE			0x04
-
-char szPort[128];
-
-int set_interface_attribs(int fd, int speed)
+int openSerialPort(char * pszPort, int speed)
 {
-	struct termios SerialPortSettings;
+	int				fd;
+	struct termios 	t;
 
-	tcgetattr(fd, &SerialPortSettings);
+	/*
+	 * Open the serial port...
+	 */
+	fd = open(pszPort, O_RDWR | O_NOCTTY | O_NDELAY);
 
-	cfsetispeed(&SerialPortSettings, speed);
-	cfsetospeed(&SerialPortSettings, speed);
+	if (fd < 0) {
+        printf("Error opening %s: %s\n", pszPort, strerror(errno));
+        return -1;
+    }
 
-	SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
-	SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-	SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
-	SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+	/*
+	 * Get current port parameters...
+	 */
+	tcgetattr(fd, &t);
 
-	SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
-	SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
+	/*
+	 * Set the baud rate...
+	 */
+	cfsetispeed(&t, speed);
+	cfsetospeed(&t, speed);
+
+	t.c_cflag &= ~PARENB;   					/* Disables the Parity Enable bit(PARENB),So No Parity   */
+	t.c_cflag &= ~CSTOPB;   					/* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+	t.c_cflag &= ~CSIZE;	 					/* Clears the mask for setting the data size             */
+	t.c_cflag |=  CS8;      					/* Set the data bits = 8                                 */
+
+	t.c_cflag &= ~CRTSCTS;       				/* No Hardware flow Control                         	 */
+	t.c_cflag |= CREAD | CLOCAL; 				/* Enable receiver,Ignore Modem Control lines            */
 
 
-	SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
-	SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+	t.c_iflag &= ~(IXON | IXOFF | IXANY);		/* Disable XON/XOFF flow control both i/p and o/p        */
+	t.c_iflag &= ~(ICANON|ECHO|ECHOE|ISIG);		/* Non Cannonical mode                                   */
 
-	SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+	t.c_oflag &= ~OPOST;						/* No Output Processing                                  */
 	
-	SerialPortSettings.c_cc[VMIN]  = 10; /* Read 10 characters */  
-	SerialPortSettings.c_cc[VTIME] = 50;  /* Wait indefinitely   */
+	t.c_cc[VMIN]  = 0;
+	t.c_cc[VTIME] = 10;  						/* Wait 1 sec                                            */
 	
-	if ((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0) {
+	/*
+	 * Set the new port parameters...
+	 */
+	if ((tcsetattr(fd, TCSANOW, &t)) != 0) {
 		printf("\n  ERROR ! in Setting attributes\n");
 		return -1;
 	}
-	
-	return 0;
-}
 
-int readSerial(const char * pszPortname, uint8_t * data)
-{
-	int					i = 0;
-	int					fd;
-    int					rlen;
-    int					bytesRead = 0;
-    int					go = 1;
-
-    fd = open(pszPortname, O_RDWR | O_NOCTTY);
-    
-	if (fd < 0) {
-        printf("Error opening %s: %s\n", pszPortname, strerror(errno));
-        return -1;
-    }
-
-	set_interface_attribs(fd, B57600);
-	
+	/*
+	 * Flush the changes...
+	 */
 	tcflush(fd, TCIFLUSH);
 
-	while (go) {
-		rlen = read(fd, &data[i], 1);
-		
-		bytesRead += rlen;
-		
-		if (errno) {
-			printf("Read error: %s\n", strerror(errno));
+	/*
+	 * Set non-blocking read...
+	 */
+	fcntl(fd, F_SETFL, FNDELAY);
+	
+	return fd;
+}
+
+int processByte(uint8_t b)
+{
+	static int 			state = RX_STATE_START;
+	static int			i;
+	static uint8_t		errorCode;
+	static RXMSGFRAME	frame;
+
+	int 				rtn = 1;
+
+	switch (state) {
+		case RX_STATE_START:
+			printf("[S]");
+			printf("[0x%02X]", b);
+			if (b == MSG_CHAR_START) {
+				frame.start = b;
+				state = RX_STATE_LENGTH;
+			}
 			break;
-		}
-		
-		if (data[i] == MSG_CHAR_END) {
-			go = 0;
-		}
-		
-		i++;
+
+		case RX_STATE_LENGTH:
+			printf("[L]");
+			printf("[%d]", b);
+			frame.cmdFrameLength = b;
+			state = RX_STATE_MSGID;
+			break;
+
+		case RX_STATE_MSGID:
+			printf("[M]");
+			printf("[0x%02X]", b);
+			frame.msgID = b;
+			state = RX_STATE_RESPTYPE;
+			break;
+
+		case RX_STATE_RESPTYPE:
+			if (b == MSG_CHAR_ACK) {
+				printf("[ACK]");
+				frame.cmd = MSG_CHAR_ACK;
+
+				if (frame.cmdFrameLength > 2) {
+					state = RX_STATE_DATA;
+					i = 0;
+				}
+				else {
+					state = RX_STATE_CHECKSUM;
+				}
+			}
+			else if (b == MSG_CHAR_NAK) {
+				printf("[NAK]");
+				frame.cmd = MSG_CHAR_NAK;
+
+				state = RX_STATE_ERRCODE;
+			}
+			break;
+
+		case RX_STATE_DATA:
+			printf("%c", b);
+			frame.data[i++] = b;
+
+			if (i == frame.cmdFrameLength - 2) {
+				state = RX_STATE_CHECKSUM;
+			}
+			break;
+
+		case RX_STATE_ERRCODE:
+			printf("[E]");
+			printf("[0x%02X]", b);
+			errorCode = b;
+			state = RX_STATE_CHECKSUM;
+			break;
+
+		case RX_STATE_CHECKSUM:
+			printf("[C]");
+			printf("[0x%02X]", b);
+			frame.checksum = b;
+			state = RX_STATE_END;
+			break;
+
+		case RX_STATE_END:
+			printf("[N]");
+			printf("[0x%02X]", b);
+			if (b == MSG_CHAR_END) {
+				frame.end = b;
+				rtn = 0;
+			}
+
+			state = RX_STATE_START;
+			break;
 	}
-	
-	close(fd);
-	
-	return bytesRead;
-}
 
-int writeSerial(const char * pszPortname, uint8_t * data, int dataLength)
-{
-	int			fd;
-	int			bytesWritten = 0;
-
-	fd = open(pszPortname, O_RDWR | O_NOCTTY);
-
-	if (fd < 0) {
-        printf("Error opening %s: %s\n", pszPortname, strerror(errno));
-        return -1;
-    }
-
-	set_interface_attribs(fd, B57600);
-
-	tcflush(fd, TCIFLUSH);
-
-	bytesWritten = write(fd, data, dataLength);
-
-	if (errno) {
-		printf("Write error: %s\n", strerror(errno));
-	}
-
-	close(fd);
-
-	return bytesWritten;
+	return rtn;
 }
 
 void * queryTPHThread(void * pArgs)
 {
 	uint8_t		msgID = 1;
 	int			go = 1;
+	int			onwards = 1;
 	int			frameLength = 0;
-	int			readLen;
+	int			writeLen;
+	int			bytesRead = 0;
 	int			i;
+	int			errorCount = 0;
+	int			zeroCount = 0;
 	uint8_t		frame[80];
+	uint8_t		b;
+	int	*		fd;
+
+	fd = (int *)pArgs;
 
 	while (go) {
 		frame[0] = MSG_CHAR_START;
@@ -188,15 +197,46 @@ void * queryTPHThread(void * pArgs)
 
 		frameLength = 6;
 
-		writeSerial(szPort, frame, frameLength);
+		writeLen = write(*fd, frame, frameLength);
 
-		usleep(500000L);
-
-		readLen = readSerial(szPort, frame);
-
-		for (i = 0;i < readLen;i++) {
-			printf("Read [0x%02X\n", frame[i]);
+		printf("Write: ");
+		for (i = 0;i < writeLen;i++) {
+			printf("[0x%02X]", frame[i]);
 		}
+		printf("\n");
+
+		errorCount = 0;
+		onwards = 1;
+
+		printf("Read: ");
+
+		while (onwards) {
+			bytesRead = read(*fd, &b, 1);
+
+			if (bytesRead > 0) {
+				onwards = processByte(b);
+				zeroCount = 0;
+			}
+			else if (bytesRead == 0) {
+				usleep(1000L);
+				zeroCount++;
+			}
+			else {
+				usleep(1000L);
+				errorCount++;
+			}
+
+			if (zeroCount == 99) {
+				zeroCount = 0;
+				onwards = 0;
+			}
+			if (errorCount == 999) {
+				onwards = 0;
+				errorCount = 0;
+			}
+		}
+
+		printf("\n");
 
 		sleep(5);
 	}
@@ -208,23 +248,32 @@ int main(int argc, char *argv[])
 {
 	int			err;
 	pthread_t	tid;
+	char		szPort[128];
+	int			fd;
 
 	if (argc > 1) {
 		strcpy(szPort, &argv[1][0]);
 	}
+	else {
+		strcpy(szPort, SERIAL_PORT);
+	}
 
-	err = pthread_create(&tid, NULL, &queryTPHThread, NULL);
+	fd = openSerialPort(szPort, B9600);
+
+	err = pthread_create(&tid, NULL, &queryTPHThread, &fd);
 
     if (err != 0) {
     	printf("\nCan't create thread :[%s]", strerror(err));
     }
     else {
-    	printf("\n Thread created successfully\n");
+    	printf("\nThread created successfully\n");
     }
 
     while (1) {
     	usleep(1000L);
     }
+
+	close(fd);
 
 	return 0;
 }
